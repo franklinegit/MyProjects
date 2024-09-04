a7e5f34564fec3205dddaabe19c1dab2141a7d42
franklinegit <oddfrankline@gmail.com>
2024-09-04 07:57:28 +0000

    Added Menu and Enabled choice from menu

--- C-Projects/Student-Database/main.c
+++ C-Projects/Student-Database/main.c
@@ -2,25 +2,52 @@
 
 void display_menu()
 {
-    printf("Enter choice of operation from the menu below.\n\n");
     printf("==== MENU OF OPERATIONS ====\n");
     printf("1. Get an integer input.\n");
     printf("2. Get a string input.\n");
     printf("3. Do nothing.\n");
-    printf("Exit the program.\n\n");
+    printf("4 .Exit the program.\n\n");
 }
 
 
 int main()
 {
-    int val;
-    const char* str;
+    while (1)
+    {
+        int choice;
+        int val;
+        const char* str;
 
-    val = get_integer_input("Enter an integer: ");
-    printf("You entered the value: %d\n\n", val);
+        display_menu();
+        choice = get_integer_input("Enter choice of operation from the menu: ");
+        printf("\n");
 
-    str = get_string_input("Enter a string:  ");
-    printf("You enterd the string: %s\n\n", str);
+        switch (choice)
+        {
+            case 1:
+                printf("Getting integer...\n");
+                val = get_integer_input("Enter an integer: ");
+                printf("You entered the value: %d\n\n", val);
+                break;
+
+            case 2:
+                printf("Getting string...\n");
+                str = get_string_input("Enter a string: ");
+                printf("You entered the string: %s\n\n", str);
+                break;
+            
+            case 3:
+                continue;
+
+            case 4:
+                printf("Exiting the program....\n");
+                exit(0);
+
+            default:
+                printf("Please Enter a valid choice between 1 and 4.\n\n");
+                break;
+        }
+    }
 
     return (0);
 }
